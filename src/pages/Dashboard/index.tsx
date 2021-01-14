51dc27beee1cf2b1ac2ac51e5216d3fdf4ba5efb
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-01-14 12:49:11 -0300

    feat: add dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,21 +1,105 @@
-import React from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
+import DayPicker from 'react-day-picker'
+import { FiPlus, FiInfo } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
-import Button from '../../components/Button'
+import { format } from 'date-fns'
+
+import logoImg from '../../assets/logo.svg'
 import { useAuth } from '../../hooks/Auth'
 
-import { Container } from './styles'
+import 'react-day-picker/lib/style.css'
+import {
+  Container,
+  Header,
+  Content,
+  Aside,
+  Calendar,
+  Main,
+  EventsList,
+  Event
+} from './styles'
 
 const Dashboard: React.FC = () => {
-  const { signOut, user } = useAuth()
+  const { user, signOut } = useAuth()
+  const [selectedDate, setSelectedDate] = useState(new Date())
+
+  const handleDateChange = useCallback((day: Date) => {
+    setSelectedDate(day)
+  }, [])
+
+  const selectedDateAsText = useMemo(() => {
+    return format(selectedDate, "'Day' dd")
+  }, [selectedDate])
+
+  const selectedWeekDay = useMemo(() => {
+    return format(selectedDate, 'cccc')
+  }, [selectedDate])
 
   return (
     <Container>
-      <h1>Hello, {user.name}!</h1>
-      <h2>The app is coming soon...</h2>
+      <Header>
+        <div>
+          <img src={logoImg} alt="DevCalendar" />
 
-      <Button onClick={signOut} loading={false}>
-        Sign Out
-      </Button>
+          <Link to="/app" onClick={signOut}>
+            <strong>{user.name}</strong>
+            <img src={user.avatarUrl} alt={user.name} />
+          </Link>
+        </div>
+      </Header>
+
+      <Content>
+        <Aside>
+          <Calendar>
+            <DayPicker
+              modifiers={{
+                available: {
+                  daysOfWeek: [0, 1, 2, 3, 4, 5, 6]
+                }
+              }}
+              selectedDays={selectedDate}
+              onDayClick={handleDateChange}
+            />
+          </Calendar>
+
+          <Link to="/app">
+            Add new Event
+            <FiPlus />
+          </Link>
+        </Aside>
+
+        <Main>
+          <div>
+            <span>Next events</span>
+
+            <p>
+              <span>{selectedDateAsText}</span>
+              <span>{selectedWeekDay}</span>
+            </p>
+          </div>
+
+          <EventsList>
+            <Event>
+              <strong>{"Mother's birthday"}</strong>
+
+              <div>
+                <span></span>
+                <FiInfo size={20} />
+              </div>
+            </Event>
+
+            <Event>
+              <strong>{"Mother's birthday"}</strong>
+
+              <div>
+                <span>06:00PM - 11:59PM</span>
+                <FiInfo size={20} />
+              </div>
+            </Event>
+          </EventsList>
+        </Main>
+      </Content>
     </Container>
   )
 }
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,14 +1,222 @@
+import { shade } from 'polished'
 import styled from 'styled-components'
 
 export const Container = styled.div`
+  height: 100vh;
+`
+
+export const Header = styled.header`
+  height: 144px;
+  background-color: ${props => props.theme.box};
+
+  > div {
+    height: 100%;
+    max-width: 1120px;
+    margin: 0 auto;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    img {
+      width: 296px;
+    }
+
+    a {
+      display: flex;
+      align-items: center;
+      color: ${props => props.theme.text};
+      text-decoration: none;
+
+      strong {
+        margin-right: 8px;
+      }
+
+      img {
+        width: 56px;
+        height: 56px;
+        border-radius: 50%;
+      }
+    }
+  }
+`
+
+export const Content = styled.div`
+  max-width: 1120px;
+  margin: 56px auto;
+
+  display: flex;
+  justify-content: space-between;
+`
+
+export const Aside = styled.aside`
+  width: 352px;
+
+  a {
+    width: 100%;
+    padding: 22px;
+    margin: 40px 0;
+    background-color: ${props => props.theme.primary};
+    border-radius: 20px;
+    color: ${props => props.theme.white};
+    text-decoration: none;
+    font-weight: 500;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      margin-left: 8px;
+    }
+  }
+`
+
+export const Calendar = styled.div`
+  .DayPicker {
+    border-radius: 10px;
+  }
+
+  .DayPicker-wrapper {
+    padding-bottom: 0;
+    background: ${props => props.theme.placeholder};
+    border-radius: 10px;
+  }
+
+  .DayPicker,
+  .DayPicker-Month {
+    width: 100%;
+  }
+
+  .DayPicker-NavBar {
+    color: ${props => props.theme.white} !important;
+  }
+
+  .DayPicker-NavButton--prev {
+    right: auto;
+    left: 1.5em;
+    margin-right: 0;
+  }
+
+  .DayPicker-Weekday {
+    color: ${props => props.theme.placeholder};
+    font-weight: 500;
+  }
+
+  .DayPicker-Month {
+    border-collapse: separate;
+    border-spacing: 8px;
+    margin: 16px 0 0 0;
+    padding: 16px;
+    background-color: ${props => props.theme.box};
+    border-radius: 0 0 10px 10px;
+  }
+
+  .DayPicker-Caption {
+    margin-bottom: 1em;
+    padding: 0 1em;
+    color: ${props => props.theme.white};
+
+    > div {
+      text-align: center;
+    }
+  }
+
+  .DayPicker-Day {
+    width: 40px;
+    height: 40px;
+  }
+
+  .DayPicker-Day--available:not(.DayPicker-Day--outside) {
+    background: ${props => props.theme.background};
+    border-radius: 10px;
+    color: ${props => props.theme.text};
+    font-weight: 500;
+  }
+
+  .DayPicker:not(.DayPicker--interactionDisabled)
+    .DayPicker-Day:not(.DayPicker-Day--disabled):not(.DayPicker-Day--selected):not(.DayPicker-Day--outside):hover {
+    background: ${props => shade(0.2, props.theme.background)};
+  }
+
+  .DayPicker-Day--today {
+    font-weight: normal;
+  }
+
+  /* .DayPicker-Day--disabled {
+    color: #666360 !important;
+    background: transparent !important;
+  } */
+
+  .DayPicker-Day--selected {
+    background: ${props => props.theme.primary} !important;
+    border-radius: 10px;
+    color: ${props => props.theme.white} !important;
+  }
+`
+
+export const Main = styled.main`
+  max-width: 641px;
+  width: 100%;
+
+  div {
+    > span {
+      font-size: 36px;
+    }
+
+    p {
+      display: flex;
+      margin-top: 8px;
+      align-items: center;
+      color: ${props => props.theme.primary};
+      font-weight: 500;
+
+      span {
+        display: flex;
+        align-items: center;
+      }
+
+      span + span::before {
+        content: '';
+        background: ${props => props.theme.primary};
+        width: 1px;
+        height: 12px;
+        margin: 0 8px;
+      }
+    }
+  }
+`
+
+export const EventsList = styled.div`
+  width: 100%;
+  margin-top: 32px;
+`
+
+export const Event = styled.div`
+  padding: 24px;
+  background: ${props => props.theme.box};
+  border-radius: 20px;
+  font-weight: 500;
+
   display: flex;
   align-items: center;
-  justify-content: center;
-  flex-direction: column;
+  justify-content: space-between;
 
-  height: 100vh;
+  div {
+    display: flex;
+    align-items: center;
 
-  button {
-    max-width: 500px;
+    span {
+      font-size: 16px;
+      color: ${props => props.theme.placeholder};
+    }
+
+    svg {
+      margin-left: 8px;
+    }
+  }
+
+  & + div {
+    margin-top: 12px;
   }
 `
